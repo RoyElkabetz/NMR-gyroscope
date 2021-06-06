2d3b52ac56b1a3b9be1f98b321a7584742674add
Roy Elkabetz <elkabetzroy@gmail.com>
2021-06-06 13:23:52 +0300

    save

--- src/physical_constant_units.py
+++ src/physical_constant_units.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # PHYSICAL CONSTANTS
 C = 2.99792458 * 1e8        # [m / s]
 
@@ -8,6 +10,8 @@ G131 = 22.0564 * 1e6        # Xenon131 [rad / s / T]  http://www.acadiau.ca/~bel
 # UNITS CONVERSIONS
 G2T = 1e-4                  # Gauss to Tesla conversion. i.e. 5 Gauss * G2T = 5e-4 Tesla
 T2G = 1e4                   # Tesla to Gauss conversion. i.e. 5 Tesla * T2G = 5e4 Gauss
+D2R = np.pi / 180           # degrees to radians conversion. i.e. 90 deg * D2R = 1.57 [rad]
+R2D = 180 / np.pi           # radians to degrees conversion. i.e. 2pi * R2D = 360 [deg]
 
 # SYMBOLS
 PI = chr(960)
