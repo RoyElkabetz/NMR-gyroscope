ee93f564803ab0f457699fd38a2190fa1896ecf2
RoyElkabetz <43312215+RoyElkabetz@users.noreply.github.com>
2021-06-21 13:00:49 +0300

    Update measurements.py

--- src/measurements.py
+++ src/measurements.py
@@ -77,7 +77,7 @@ def single_species_Open_Loop_bandwidth_simualtion(gyromagnetic, t1, t2, wr_amp=0
             my_Xe.plot_results(my_env)
             
         if plot_steps_PSD:
-            signle_list = [my_Xe.gamma * (B0 + Bnoise), wr]
+            signals_list = [my_Xe.gamma * (B0 + Bnoise), wr]
             names = [r'$\gamma B$', r'$\Omega_r$']
             utils.psd_compare(signals_list, sampling_frequency_hz, noise_amplitude=Bnoise_amp, names=names)
 
