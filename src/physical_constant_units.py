3bb3f5e296a0f97f58af84fed4012c8fa6a4fb95
Roy Elkabetz <43312215+RoyElkabetz@users.noreply.github.com>
2021-07-26 09:50:09 +0300

    rubidium nuclear spins and gyromagnetic ratios

--- src/physical_constant_units.py
+++ src/physical_constant_units.py
@@ -3,9 +3,16 @@ import numpy as np
 # PHYSICAL CONSTANTS
 C = 2.99792458 * 1e8        # [m / s]
 
+# SPINS
+I85 = 5 / 2                 # Rubidium 85 Nuclear Spin (I)
+I87 = 3 / 2                 # Rubidium 87 Nuclear Spin (I)
+
 # GYROMAGNETIC RATIOS
 G129 = -74.4069 * 1e6       # Xenon129 [rad / s / T]  http://www.acadiau.ca/~bellis/resources/nmr/isotopes.html
 G131 = 22.0564 * 1e6        # Xenon131 [rad / s / T]  http://www.acadiau.ca/~bellis/resources/nmr/isotopes.html
+Ge = 1.760859644 * 1e11     # isolated electron [rad / s / T]  https://en.wikipedia.org/wiki/Gyromagnetic_ratio
+G85 = Ge / (2 * I85 + 1)    # Rubidium 85 ESR gyromagnetic ratio [rad / s / T]
+G87 = Ge / (2 * I87 + 1)    # Rubidium 85 ESR gyromagnetic ratio [rad / s / T]
 
 # UNITS CONVERSIONS
 G2T = 1e-4                  # Gauss to Tesla conversion. i.e. 5 Gauss * G2T = 5e-4 Tesla
